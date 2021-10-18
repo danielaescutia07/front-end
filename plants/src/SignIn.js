df8fedc72b318a63df9107d3126f739d412f70df
danielaescutia07 <danielaescutia@yahoo.com>
2021-10-18 12:49:17 -0500

    home page is done

---
+++ plants/src/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignIn = () => {
+
+    return (
+        <div className='sign-in'>
+            <h3>Sign In Form</h3>
+        </div>
+    )
+}
+
+export default SignIn;
